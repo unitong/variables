a5af8c16cfd2cc16669e68170e1b16e84f600e59
Sophia Tang <Finenyaco@users.noreply.github.com>
2023-12-12 19:35:14 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,21 +6,9 @@ variable "test_options" {
 variable "instance_iam_role_policies" {
   description = "IAM policies to attach to the IAM role"
   type        = map(string)
-  default     = {}
+  default     = { test: map123 }
 }
 
-variable "test_tuple" {
-  description = "Testing tuple"
-  type        = tuple([string, number, bool])
-  default     = ("hello", 42, true)
-}
-
-
-variable "test_set" {
-  description = "Testing tuple"
-  type        = set(string)
-  default     = ["apple", "banana", "orange"]
-}
 
 output "test_options_output" {
   value       = var.test_options
@@ -28,12 +16,4 @@ output "test_options_output" {
 
 output "instance_iam_role_policies_output" {
   value       = var.instance_iam_role_policies
-}
-
-output "tuple_output" {
-  value       = var.test_tuple
-}
-
-output "tuple_set" {
-  value       = var.test_set
 }
