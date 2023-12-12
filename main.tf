892d6e46530cd8973a3e60be620ccb40fa79208b
Sophia Tang <Finenyaco@users.noreply.github.com>
2023-12-12 19:38:11 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 variable "test_options" {
   description = "Testing options list and no default value"
   type        = list(string)
+  default     = ["apple", "banana"]
 }
 
 variable "instance_iam_role_policies" {
   description = "IAM policies to attach to the IAM role"
   type        = map(string)
-  default     = { test: map123 }
+  default     = {}
 }
 
 
